38eb04cb966a00179388f5329ffea4ef3583251d
shengsixiang <shengsixiang@meicai.cn>
2017-03-17 18:44:49 +0800

    update port

--- demo1/demo.py
+++ demo1/demo.py
@@ -14,7 +14,7 @@ from tornado.options import define, options
 
 #import pymongo
 
-define("port", default=8002, help="run on the given port", type=int)
+define("port", default=8001, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
 	def __init__(self):
